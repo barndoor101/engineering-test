045ca1ac235995eee4fd4aebd8880d995d44ccd9
Andrew Berryman <andrew.berryman@gmail.com>
2024-04-04 03:10:46 +0100

    Update models.ts

--- eurocamp.web/src/app/models/models.ts
+++ eurocamp.web/src/app/models/models.ts
@@ -1,25 +1,19 @@
 export class Users {
-
     id: string;
     name: string;
     email: string;
-
 }
 
 export class Parcs {
-
     id: string;
     name: string;
-    description: string;
-    
+    description: string; 
 }
 
 export class Bookings {
-
     id: string;
     user: string;
     parc: string;
     bookingdate: Date;
     comments: string;
-
 }
